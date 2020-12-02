3bfe15beea7ee6b536dbadaa7bcbd0c8245b03ea
Sumit Jha <sj@comfnet.de>
2020-12-02 16:41:37 +0100

    Initial Commit

---
+++ adventofcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  adventofcode
+//
+//  Created by Sumit Jha on 02.12.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
